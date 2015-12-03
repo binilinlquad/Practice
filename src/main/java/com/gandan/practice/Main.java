f9af152b28c2f97c56ffe2006455d5c5baa7f024
Chandra Yang <otama.ariq@gmail.com>
2015-12-03 21:35:38 +0700

    add test return in try, catch, and finally

--- src/main/java/com/gandan/practice/Main.java
+++ src/main/java/com/gandan/practice/Main.java
@@ -1,12 +1,13 @@
 package com.gandan.practice;
 
 import com.gandan.practice.task.*;
+import com.gandan.practice.trycatch.TryCatchFinally;
 
 public class Main {
 
     public static void main(String args[]) {
         ITask[] practices = {new TaskFactory(), new TaskFibonnaci(),
-                new TaskReflection(), new TaskSemaphore()};
+                new TaskReflection(), new TaskSemaphore(), new TryCatchFinally()};
         for (ITask practice : practices) {
             practice.execute();
         }
