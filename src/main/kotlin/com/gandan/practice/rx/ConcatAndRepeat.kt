35fd3ecaefc5c22fadea3ee275b0c2f7756533d6
Chandra Yang <otama.ariq@gmail.com>
2016-09-02 00:16:03 +0700

    Repeat with correct way (use concat for testing the right behavior)

---
+++ src/main/kotlin/com/gandan/practice/rx/ConcatAndRepeat.kt
@@ -0,0 +1,76 @@
+package com.gandan.practice.rx
+
+import rx.Notification
+import rx.Observable
+
+fun main(args: Array<String>) {
+    // warming up with range
+    System.out.println("Range")
+    val a = Observable.range(1, 10)
+    val b = Observable.range(50, 10)
+
+    a.concatWith(b).subscribe { System.out.print("$it ") }
+    System.out.print(" It's correct")
+
+    System.out.println("\nRepeat with counter")
+    val c = Observable.just("c").repeat(10)
+    val d = Observable.just("d").repeat(10)
+
+    c.concatWith(d).subscribe { System.out.print("$it ") }
+    System.out.print(" It's correct")
+
+    System.out.println("\nRepeat with flatmap")
+    var flagE = false
+    var flagF = false
+    val e = Observable.just("e").repeatWhen {
+        it.flatMap {
+            if (!flagE) {
+                flagE = true
+                Observable.just("$it 0")
+            } else {
+                Observable.empty()
+            }
+        }
+    }
+    val f = Observable.just("f").repeatWhen {
+        it.flatMap {
+            if (!flagF) {
+                flagF = true
+                Observable.just("$it 1")
+            } else {
+                Observable.empty()
+            }
+        }
+    }
+
+    e.concatWith(f).subscribe { System.out.print("$it ") }
+    System.out.print(" It's wrong")
+
+    System.out.println("\nRepeat with flatmap 2")
+    var flagG = false
+    val g = Observable.just("g").repeatWhen { source ->
+        source.map {
+            if (!flagG) {
+                flagG = true
+                Notification.createOnNext(Unit)
+            } else {
+                Notification.createOnCompleted<Unit>()
+            }
+        }.dematerialize<Unit>()
+    }
+
+    var flagH = false
+    val h = Observable.just("h").repeatWhen { source ->
+        source.map() {
+            if (!flagH) {
+                flagH = true
+                Notification.createOnNext(Unit)
+            } else {
+                Notification.createOnCompleted<Unit>()
+            }
+        }.dematerialize<Unit>()
+    }
+
+    g.concatWith(h).subscribe { System.out.print("$it ") }
+    System.out.print(" It's correct")
+}
