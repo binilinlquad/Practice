a804a8272f3f0c167e6e0dc7087aded3c0d467bd
Chandra Yang <otama.ariq@gmail.com>
2016-09-19 22:50:22 +0700

    play with materialize

--- src/main/kotlin/com/gandan/practice/rx/MaterializeAndLog.kt
+++ src/main/kotlin/com/gandan/practice/rx/MaterializeAndLog.kt
@@ -4,7 +4,9 @@ import rx.Observable
 
 fun main(args: Array<String>) {
     Observable.range(1, 10)
-            .compose { t -> t.apply { materialize().subscribe { System.out.println("${it.kind} ${it.value}") } } }
+            .compose { t -> t.apply { materialize()
+                    .doOnCompleted { System.out.println("Complete") }
+                    .subscribe { System.out.println("${it.kind} ${it.value}") } } }
             .subscribe { System.out.println(it) }
-
 }
+
