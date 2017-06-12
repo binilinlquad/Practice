85c7f06341514705d7246f7b79dc8dea48da81ba
Chandra Yang <otama.ariq@gmail.com>
2017-06-12 20:54:54 +0700

    Check Flowable scan behavior against Obsrvable scan

--- src/main/kotlin/com/gandan/practice/rx2/Scan.kt
+++ src/main/kotlin/com/gandan/practice/rx2/Scan.kt
@@ -6,28 +6,30 @@ import io.reactivex.Observable
 
 fun main(args: Array<String>) {
 
-    // It has different behavior with Observable scan with initial value behavior,
+    // Flowable scan has different behavior with Observable scan with initial value behavior,
     // it will emit first item when publisher emit its first value
     System.out.println("Flowable")
-            Flowable.fromArray(1L,2L,3L)
+    Flowable.fromArray(1L, 2L, 3L)
             .log<Long>()
-            .scan(emptyList<Long>(), { list, value -> list + value})
+            .scan(emptyList<Long>(), { list, value -> list + value })
             .doOnNext { System.out.println("$it") }.subscribe()
 
 
+    // Flowable scan without initial value will work like Observable scan with initial value
     System.out.println("Flowable scan without initial value")
-    Flowable.fromArray(1L,2L,3L)
+    Flowable.fromArray(1L, 2L, 3L)
             .map { listOf(it) }
             .log<List<Long>>()
-            .scan({ list, value -> list + value})
+            .scan({ list, value -> list + value })
             .doOnNext { System.out.println("$it") }.subscribe()
 
     System.out.println("Observable")
 
-            Observable.fromArray(1L, 2L, 3L)
+    // Observable scan with initial value will emit initial value when subscribed
+    Observable.fromArray(1L, 2L, 3L)
             .log<Long>()
-            .scan(emptyList<Long>(), {list, value -> list + value})
+            .scan(emptyList<Long>(), { list, value -> list + value })
             .doOnNext { System.out.println("$it") }.subscribe()
 
-    Thread.sleep(3000)
+    Thread.sleep(5000)
 }
