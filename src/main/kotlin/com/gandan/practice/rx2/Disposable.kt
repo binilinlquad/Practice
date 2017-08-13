1efb64086d6d783956050181436d61aa66591421
Chandra Yang <otama.ariq@gmail.com>
2017-08-13 20:02:08 +0700

    Check diposable behavior

--- src/main/kotlin/com/gandan/practice/rx2/Disposable.kt
+++ src/main/kotlin/com/gandan/practice/rx2/Disposable.kt
@@ -5,19 +5,30 @@ import io.reactivex.Flowable
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
-/**
- * Cancel method is called at the end of emit evntes before dispose happened.
- * Dispose only happened when dispose() called.
- */
 fun main(args: Array<String>) {
+    /**
+     * Taken from stackoverflow by David Karnok
+     * https://stackoverflow.com/questions/43280248/what-is-the-difference-between-rxjava-2-cancellable-and-disposable
+     *
+     * [Cancellable is] A functional interface that has a single cancel method that can throw.
+     * The Disposable is not a functional interface plus when implementing its dispose() method, you are not allowed to throw checked exceptions.
+     */
+
     val s = Flowable.create<Int>({
-        emitter -> (1..10).forEach {
-        emitter.onNext(it) }
+        emitter ->
+        (1..10).forEach {
+        Thread.sleep(1000)
+        emitter.onNext(it)
+    }
         emitter.setCancellable { System.err.println("Cancelled") }
+        (11..15).forEach {
+            Thread.sleep(1000)
+            emitter.onNext(it)
+        }
         emitter.setDisposable(object : Disposable {
             override fun isDisposed(): Boolean {
                 System.err.println("IsDisposed?")
-                return false
+                return true
             }
 
             override fun dispose() {
@@ -32,8 +43,8 @@ fun main(args: Array<String>) {
     }
 
     System.err.println("Sleep 2000")
-    Thread.sleep(2000)
+    Thread.sleep(20000)
     System.err.println("disposing")
-    s.dispose()
+    s.dispose() // if not called, then it wont dispose
 
 }
