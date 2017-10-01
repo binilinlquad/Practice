95273d55479bfd382693154a996a0a7ddbd0b0ee
Chandra Yang <otama.ariq@gmail.com>
2017-10-01 20:45:48 +0700

    sample folding

--- src/main/kotlin/com/gandan/practice/kotlin/Folding.kt
+++ src/main/kotlin/com/gandan/practice/kotlin/Folding.kt
@@ -1,5 +1,11 @@
 package com.gandan.practice.kotlin
 
-/**
- * Created by chandra on 11/28/16.
- */
+fun main(args: Array<String>) {
+    val a = listOf(1, 2, 3, 4, 5)
+    a.fold(emptyList<Int>(), { result, element ->
+        System.err.println("Before $result")
+        val r = result + (result.lastOrNull() ?: element)
+        System.err.println("After $r")
+        r
+    } )
+}
