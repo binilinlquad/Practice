58bbbad01f13298cb98cc918fbf78bef50988791
Chandra Yang <otama.ariq@gmail.com>
2018-01-26 02:56:26 +0700

    prefer provided methods (doOnNext, doOnError, etc) to materialize-dematerialize for loggin

--- src/main/kotlin/com/gandan/practice/common/Rx2Utils.kt
+++ src/main/kotlin/com/gandan/practice/common/Rx2Utils.kt
@@ -4,20 +4,19 @@ import io.reactivex.Flowable
 import io.reactivex.Observable
 
 
-fun <T> Flowable<T>.log() : Flowable<T> {
-    return materialize().doOnNext {
-        if (it.isOnNext) System.out.println(it.value)
-        else if (it.isOnError) System.err.println(it.value)
-        else if (it.isOnComplete) System.out.println("Completed")
-    }
-            .dematerialize<T>()
+fun <T> Flowable<T>.log(tag: String="") : Flowable<T> {
+    return this.doOnNext { System.out.println("$tag onNext: $it") }
+            .doOnError { System.err.println("$tag onError $it") }
+            .doOnComplete { System.out.println("$tag Completed")}
+            .doOnSubscribe { System.out.println("$tag Subscribed") }
+            .doOnTerminate { System.out.println("$tag Terminated") }
 }
 
-fun <T> Observable<T>.log() : Observable<T> {
-    return materialize().doOnNext {
-        if (it.isOnNext) System.out.println(it.value)
-        else if (it.isOnError) System.err.println(it.value)
-        else if (it.isOnComplete) System.out.println("Completed")
-    }
-            .dematerialize<T>()
+fun <T> Observable<T>.log(tag: String = "") : Observable<T> {
+    return this.doOnNext { System.out.println("$tag onNext: $it") }
+            .doOnError { System.err.println("$tag onError $it") }
+            .doOnComplete { System.out.println("$tag Completed")}
+            .doOnSubscribe { System.out.println("$tag Subscribed") }
+            .doOnTerminate { System.out.println("$tag Terminated") }
+            .doOnDispose { System.out.println("$tag Disposed") }
 }
