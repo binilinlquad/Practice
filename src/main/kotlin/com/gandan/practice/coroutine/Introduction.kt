2c382fba10cb475a191946362270e456a448e34a
Chandra Yang <otama.ariq@gmail.com>
2018-05-04 01:16:43 +0700

    explicitly show return type of async

--- src/main/kotlin/com/gandan/practice/coroutine/Introduction.kt
+++ src/main/kotlin/com/gandan/practice/coroutine/Introduction.kt
@@ -1,8 +1,6 @@
 package com.gandan.practice.coroutine
 
-import kotlinx.coroutines.experimental.CommonPool
-import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.runBlocking
+import kotlinx.coroutines.experimental.*
 import kotlin.coroutines.experimental.buildSequence
 
 fun main(args: Array<String>) {
@@ -38,9 +36,29 @@ fun main(args: Array<String>) {
         }
     }
 
+    val assync : Deferred<Int> = async(CommonPool) {
+        val threadId = Thread.currentThread().id
+        System.out.println("1 with async in $threadId")
+        Thread.sleep(5000)
+        System.out.println("2 with async in $threadId")
+        5
+    }
+
     // blocked by Thread.sleep inside runBlocking
     val threadId = Thread.currentThread().id
     System.out.println("5 line main in $threadId")
     System.out.println("Fibs ${fibonacci.take(10).joinToString()}")
     System.out.println("finish line main in $threadId")
+
+    runBlocking {
+        // What difference between join and await
+        // https://stackoverflow.com/questions/46226518/what-is-the-difference-between-launch-join-and-async-await-in-kotlin-coroutines
+        // basically join return Unit
+        // assync.join()
+
+        // await return value (integer here)
+        System.out.println("async result : ${assync.await()}")
+
+        System.out.println("Line after async")
+    }
 }
