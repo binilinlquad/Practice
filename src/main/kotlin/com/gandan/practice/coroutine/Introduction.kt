c96e16b98870f66fdba0e25b0e05f0897840dab0
Chandra Yang <otama.ariq@gmail.com>
2018-05-09 01:25:47 +0700

    extract into functions and add sample for inherint parent coroutine context and the effect

--- src/main/kotlin/com/gandan/practice/coroutine/Introduction.kt
+++ src/main/kotlin/com/gandan/practice/coroutine/Introduction.kt
@@ -4,25 +4,54 @@ import kotlinx.coroutines.experimental.*
 import kotlin.coroutines.experimental.buildSequence
 
 fun main(args: Array<String>) {
-    // block will run in different thread
-    val coroutine = launch(CommonPool) {
-        val threadId = Thread.currentThread().id
-        System.out.println("1 with launch in $threadId")
-        Thread.sleep(5000)
-        System.out.println("2 with launch in $threadId")
-    }
-    coroutine.start()
+    sampleLaunchInCommonPool()
 
     // block run in same thread with current
-    runBlocking {
-        val threadId = Thread.currentThread().id
-        System.out.println("3 with runBlocking in $threadId")
-        Thread.sleep(5000)
-        System.out.println("4 with runBlocking in $threadId")
-    }
+    sampleBlocking()
 
     // shamelessly copied from kotlin https://github.com/Kotlin/kotlin-coroutines/blob/master/kotlin-coroutines-informal.md#asynchronous-computations
     // block sequence will run in same thread with current thread
+    val fibonacci = sampleBuildSequence()
+
+    // blocked by Thread.sleep inside runBlocking
+    val threadId = Thread.currentThread().id
+    System.out.println("5 line main in $threadId")
+    System.out.println("Fibs ${fibonacci.take(10).joinToString()}")
+    System.out.println("finish line main in $threadId")
+
+    val assync: Deferred<Int> = sampleAsynCommonPool()
+
+    sampleJointAndAwait(assync)
+
+    sampleInherintParentContext()
+}
+
+private fun sampleJointAndAwait(assync: Deferred<Int>) {
+    runBlocking {
+        // What difference between join and await
+        // https://stackoverflow.com/questions/46226518/what-is-the-difference-between-launch-join-and-async-await-in-kotlin-coroutines
+        // basically join return Unit
+        // assync.join()
+
+        // await return value (integer here)
+        System.out.println("async result : ${assync.await()}")
+
+        System.out.println("Line after async")
+    }
+}
+
+private fun sampleAsynCommonPool(): Deferred<Int> {
+    val assync: Deferred<Int> = async(CommonPool) {
+        val threadId = Thread.currentThread().id
+        System.out.println("1 with async in $threadId")
+        Thread.sleep(5000)
+        System.out.println("2 with async in $threadId")
+        5
+    }
+    return assync
+}
+
+private fun sampleBuildSequence(): Sequence<Int> {
     val fibonacci = buildSequence<Int> {
         System.out.println("Sequence in ${Thread.currentThread().id}")
         yield(1) // first Fibonacci number
@@ -35,30 +64,48 @@ fun main(args: Array<String>) {
             next = tmp
         }
     }
+    return fibonacci
+}
 
-    val assync : Deferred<Int> = async(CommonPool) {
-        val threadId = Thread.currentThread().id
-        System.out.println("1 with async in $threadId")
-        Thread.sleep(5000)
-        System.out.println("2 with async in $threadId")
-        5
-    }
-
-    // blocked by Thread.sleep inside runBlocking
-    val threadId = Thread.currentThread().id
-    System.out.println("5 line main in $threadId")
-    System.out.println("Fibs ${fibonacci.take(10).joinToString()}")
-    System.out.println("finish line main in $threadId")
-
+private fun sampleBlocking() {
     runBlocking {
-        // What difference between join and await
-        // https://stackoverflow.com/questions/46226518/what-is-the-difference-between-launch-join-and-async-await-in-kotlin-coroutines
-        // basically join return Unit
-        // assync.join()
-
-        // await return value (integer here)
-        System.out.println("async result : ${assync.await()}")
-
-        System.out.println("Line after async")
+        val threadId = Thread.currentThread().id
+        System.out.println("3 with runBlocking in $threadId")
+        Thread.sleep(5000)
+        System.out.println("4 with runBlocking in $threadId")
     }
+}
+
+private fun sampleLaunchInCommonPool() {
+    // block will run in different green thread
+    val coroutine = launch(CommonPool) {
+        val threadId = Thread.currentThread().id
+        System.out.println("1 with launch in $threadId")
+        Thread.sleep(5000)
+        System.out.println("2 with launch in $threadId")
+    }
+    coroutine.start()
+}
+
+// inherit parent coroutine scope
+fun sampleInherintParentContext() {
+    val pretendCalculationAsync = async(CommonPool) {
+        // suspend this coroutine, it will let caller continue
+        Thread.sleep(1000)
+
+        // child coroutine will be not runned because we cancel
+        // parent coroutine in next line after async and
+        // child inherint parent coroutine scope
+        val pretendCalculationInside = async(coroutineContext) {
+            // never printed
+            System.out.println("Child coroutine")
+        }
+        // never printed
+        System.out.println("Parent coroutine")
+
+    }
+
+    // to show that by doing it, it will cancel child too
+    pretendCalculationAsync.cancel()
+
 }
