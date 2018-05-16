fed38206efcc5205bc6e4b974f454bdaf8745ef0
Chandra Yang <otama.ariq@gmail.com>
2018-05-16 23:50:08 +0700

    fix logic for fibonacci using operator and mapper

--- src/main/kotlin/com/gandan/practice/fibonacci/RxFib.kt
+++ src/main/kotlin/com/gandan/practice/fibonacci/RxFib.kt
@@ -1,31 +1,59 @@
 package com.gandan.practice.fibonacci
 
-import com.gandan.practice.common.log
 import rx.Observable
 import rx.Subscriber
+import rx.functions.Func1
 
 fun main(args: Array<String>) {
-    Observable.just(10, 2).lift(OperatorFib()).take(3)
-            .subscribe { System.err.println(it) }
+    // print fibronnaci series using custom operator
+    Observable
+            .range(0, 20)
+            .take(100)
+            .lift(OperatorFib())
+            .subscribe { System.out.println("Use Operator $it") }
+
+    // print fibronnaci series using mapper
+    Observable
+            .range(0, 100)
+            .take(20)
+            .map(MapToFibonacci())
+            .subscribe {
+                System.out.println("Use Mapper $it")
+            }
 }
 
-internal class OperatorFib() : Observable.Operator<Int, Int> {
+// this class will map each call with next fibonacci
+// input will be ignored
+private class MapToFibonacci : Func1<Int, Int> {
+    var first = 0
+    var second = 1
 
+    override fun call(t: Int): Int {
+        val curr = first
+        first = second
+        second += curr
+        return curr
+    }
+}
+
+// this is experiment with Operator. For proper implementation,
+// I suggest to check take operator
+private class OperatorFib() : Observable.Operator<Int, Int> {
     override fun call(child: Subscriber<in Int>?): Subscriber<in Int> {
-        val parent = object: Subscriber<Int>() {
+        val parent = object : Subscriber<Int>() {
+            var first = 0
+            var second = 1
+
             override fun onCompleted() {
                 child?.onCompleted()
             }
 
             override fun onNext(step: Int?) {
-                if (!isUnsubscribed() && step != null) {
-                    var first = 0
-                    var second = 1
-                    (0..step-1).forEach {
-                        child?.onNext(first)
-                        first = first + second
-                        second = first - second
-                    }
+                if (!isUnsubscribed && step != null) {
+                    child?.onNext(first)
+                    val curr = first
+                    first = second
+                    second += curr
                 }
             }
 
