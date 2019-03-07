fe847a88869e04175342ee0a4cc1d3eb329b0e65
Chandra Yang <otama.ariq@gmail.com>
2019-03-07 20:27:46 +0700

    Improve log and add informative comment above moving coroutine context
    If we didn't define Dispatcher when using async, it will inherit coroutine
    parent context.

--- src/main/kotlin/com/gandan/practice/coroutine/AsyncCoroutine.kt
+++ src/main/kotlin/com/gandan/practice/coroutine/AsyncCoroutine.kt
@@ -1,12 +1,17 @@
 package com.gandan.practice.coroutine
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 
 
 fun main() {
     runBlocking {
+        // if we didn't define coroutine context where async is running, it will inherit context
+        // from parent context.
+        // we could move it by pass Dispatcher to async
         val longOperation = async {
+            System.out.println("Async in thread ${Thread.currentThread()}")
             Thread.sleep(1000)
             10
         }
@@ -18,11 +23,11 @@ fun main() {
         val start = System.currentTimeMillis()
         val firstCall = longOperation.await()
         val end = System.currentTimeMillis()
-        System.out.println("First operation returns ${firstCall} in ${end - start}ms")
+        System.out.println("First time invocation long operation coroutine returns ${firstCall} in ${end - start}ms")
 
         val start2 = System.currentTimeMillis()
         val secondCall = longOperation.await()
         val end2 = System.currentTimeMillis()
-        System.out.println("Second operation return ${secondCall} in ${end2 - start2}ms")
+        System.out.println("Second time invocation long operation cotourine return ${secondCall} in ${end2 - start2}ms")
     }
 }
