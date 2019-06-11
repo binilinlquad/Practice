b0cbd4c0b73c1edb41caea58bda46d7e783e1644
Chandra Yang <otama.ariq@gmail.com>
2019-06-11 22:32:21 +0700

    small refactor and try to measuring times needed for atomic integer and lock object

--- src/main/kotlin/com/gandan/practice/racing/RacingCondition.kt
+++ src/main/kotlin/com/gandan/practice/racing/RacingCondition.kt
@@ -2,15 +2,37 @@ package com.gandan.practice.racing
 
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.atomic.AtomicInteger
+import kotlin.system.measureTimeMillis
 
 fun main() {
 
-    for (i in 1..1_000_000) {
-        // race()
-//        atomicIntegerForAvoidingRaceCondition()
-        lockForAvoidingRaceCondition()
+    // uncomment this to simulate race condition
+//    measureTimeMillis {
+//        repeat(1_000_000) { race() }
+//    }
+
+    repeat(1 /* 100 */) {
+        // solution using atomic
+        val measuredTimeForAtomicSolution = measureTimeMillis {
+            repeat(1_000_000) { atomicIntegerForAvoidingRaceCondition() }
+        }
+
+        val measuredTimeForLockSolution = measureTimeMillis {
+            repeat(1_000_000) { lockForAvoidingRaceCondition() }
+        }
+
+        System.out.println("times for atomic integer : $measuredTimeForAtomicSolution and lock object: $measuredTimeForLockSolution")
     }
+
+    // Result of repeating 5 times
+//    times for atomic integer : 174363 and lock object: 172566
+//    times for atomic integer : 190477 and lock object: 176733
+//    times for atomic integer : 196610 and lock object: 179015
+//    times for atomic integer : 174998 and lock object: 178198
+//    times for atomic integer : 176344 and lock object: 181678
+
 }
+
 
 private fun race() {
     var counter = 0
